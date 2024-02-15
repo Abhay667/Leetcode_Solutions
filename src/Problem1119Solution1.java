a90112bc00c3f53dabb47b397a63e292c9b733ba
Abhay667 <abhayagrawal.cmp@gmail.com>
2024-02-15 10:25:51 +0530

    kj

---
+++ src/Problem1119Solution1.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class Problem1119Solution1 {
+    public static String Remove(String s) {
+        StringBuffer result = new StringBuffer();
+        HashSet<Character> vowels = new HashSet<>();
+        // inserting vowels in HashSet
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('u');
+
+        for (char ch : s.toCharArray()) {
+            //System.out.println(ch);
+            if(!vowels.contains(ch))
+            {
+                result.append(ch);
+            }
+        }
+return result.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String s = "abcdefghi";
+
+        System.out.println(Remove(s));
+    }
+}
